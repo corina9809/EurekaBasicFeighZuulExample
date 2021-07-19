efadde1c378a614b08bcd88bdb6fdb18cb981d85
corina9809 <mypost333@web.de>
2021-07-19 11:45:39 +0200

    Zuul-Feighn Third Commit

--- FeignClient/src/main/java/feignclient/GreetingClient.java
+++ FeignClient/src/main/java/feignclient/GreetingClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value="spring-cloud-eureka-client", url="http://localhost:8081/getVersion")
+@FeignClient(value="spring-cloud-eureka-client", url="http://localhost:8005/getVersion")
 public interface GreetingClient {
     @GetMapping
     String greeting();
